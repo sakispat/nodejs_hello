fabd57f9c655d78f25502c23a79e8de2cff5c6a7
Sakis Pat <sakispatralis@gmail.com>
2021-02-11 14:44:07 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Node.js
+
+## Tutorial
+This is the Hello World in node.js
+
+```bash
+    # run server
+    npm run dev
+```
---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res) => res.send('Hello World!!!'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
